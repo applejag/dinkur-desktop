57764af5967db4ad100fb11d31b85cfe9b4365a3
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2023-02-10 17:15:34 +0100

    Play around with carbon

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,66 @@
 <script>
-	import 'carbon-components-svelte/css/g80.css';
+	import 'carbon-components-svelte/css/g90.css';
 	import './styles.css';
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+		HeaderNavMenu,
+		SideNav,
+		SideNavItems,
+		SideNavMenu,
+		SideNavMenuItem,
+		SideNavLink,
+		SideNavDivider,
+		SkipToContent,
+		Content,
+		Grid,
+		Row,
+		Column,
+	} from "carbon-components-svelte";
+	import Fade from "carbon-icons-svelte/lib/Fade.svelte";
+  
+	let isSideNavOpen = false;
 </script>
 
-<div class="app">
-	<header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer />
-</div>
+<Header company="Dinkur" platformName="Time tracking" bind:isSideNavOpen>
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+	<HeaderNav>
+		<HeaderNavItem href="/" text="Link 1" />
+		<HeaderNavItem href="/" text="Link 2" />
+		<HeaderNavItem href="/" text="Link 3" />
+		<HeaderNavMenu text="Menu">
+			<HeaderNavItem href="/" text="Link 1" />
+			<HeaderNavItem href="/" text="Link 2" />
+			<HeaderNavItem href="/" text="Link 3" />
+		</HeaderNavMenu>
+		<HeaderNavItem href="/" text="Link 4" />
+	</HeaderNav>
+</Header>
+  
+<SideNav bind:isOpen={isSideNavOpen} fixed>
+	<SideNavItems>
+		<SideNavLink icon={Fade} text="Link 1" href="/" isSelected />
+		<SideNavLink icon={Fade} text="Link 2" href="/" />
+		<SideNavLink icon={Fade} text="Link 3" href="/" />
+		<SideNavMenu icon={Fade} text="Menu">
+			<SideNavMenuItem href="/" text="Link 1" />
+			<SideNavMenuItem href="/" text="Link 2" />
+			<SideNavMenuItem href="/" text="Link 3" />
+		</SideNavMenu>
+		<SideNavDivider />
+		<SideNavLink icon={Fade} text="Link 4" href="/" />
+	</SideNavItems>
+</SideNav>
+  
+<Content>
+	<Grid>
+		<Row>
+			<Column>
+				<slot/>
+			</Column>
+		</Row>
+	</Grid>
+</Content>
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		// Vite & Svelte preprocessors
+		vitePreprocess(),
+		// https://github.com/carbon-design-system/carbon-preprocess-svelte
+		optimizeImports()
+	],
 
 	kit: {
 		adapter: adapter({
